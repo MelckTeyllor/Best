6741cb2b52092f000cb11c460cd241885b34c90b
Melcksedeck Teyllor <melcksedeck123@gmail.com>
2024-05-17 23:24:04 -0300

    project add

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,31 @@
+import {Link} from "react-router-dom";
+import PropTypes from "prop-types";
+import{FaStar} from "react-icons/fa"
+
+const imageURL = import.meta.env.VITE_IMG;
+
+const MovieCard = ({movie, showLink = true}) => {
+  return (
+    <div className="movie-card">
+       <img src={imageURL + movie.poster_path} alt={movie.title} />
+        <h2>{movie.title}</h2>
+        <p>
+            <FaStar /> {movie.vote_average}
+        </p>
+
+        {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
+    </div>
+  )
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    poster_path: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    vote_average: PropTypes.number.isRequired,
+  }).isRequired,
+  showLink: PropTypes.bool,
+};
+
+export default MovieCard;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import {Link, Outlet, useNavigate} from 'react-router-dom'
+import { BiCameraMovie, BiSearchAlt2} from "react-icons/bi"
+import { useState } from 'react';
+
+import './Navbar.css'
+
+const Navbar = () => {
+  const [search, setSearch] = useState("")
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(!search) return
+
+    navigate(`/search?q=${search}`);
+    setSearch("")
+  }
+
+  return (
+    <div>
+        <nav id="navbar">
+          <h2> <Link to="/" > <BiCameraMovie/>BestMovies</Link></h2>
+          <form onSubmit={handleSubmit}>
+            <input type="text" placeholder='Busque um filme' onChange={(e) => setSearch(e.target.value)} 
+            value={search}
+            />
+            <button type='submit'> <BiSearchAlt2/> </button>
+          </form>
+        </nav>
+        <Outlet/>
+      
+    </div>
+  )
+}
+
+export default Navbar
